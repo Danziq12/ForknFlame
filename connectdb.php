21013cc602bc78186eef87bf274ccae3587823ca
Danziq12 <danish.ziqry0512@gmail.com>
2026-08-28 02:07:26 +0800

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -3,10 +3,10 @@ $servername = "maglev.proxy.rlwy.net";
 $username = "root";
 $password = "AOUrkudaIYJYTZTCuSrXkcKfCOzYZWid";
 $dbname = "railway";
-$port = "23881";
+$port = 23881;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $23881);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
