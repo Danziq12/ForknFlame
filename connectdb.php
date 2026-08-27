92bfdbd47d99ec2a8ee7795da17ac1bee8d0b667
Danziq12 <danish.ziqry0512@gmail.com>
2026-08-28 02:01:49 +0800

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
+$servername = "maglev.proxy.rlwy.net";
 $username = "root";
-$password = "";
-$dbname = "forkandflame";
+$password = "AOUrkudaIYJYTZTCuSrXkcKfCOzYZWid";
+$dbname = "railway";
+$port = "23881";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $23881);
 
 // Check connection
 if ($conn->connect_error) {
