024b61eaab5fc4383408343ad366a441f7ed899d
Danziq12 <danish.ziqry0512@gmail.com>
2026-08-28 18:16:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -98,26 +98,26 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg sticky-top navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Fork & Flame</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarContent">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="Menu.php">Menu</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Booking.php">Booking</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="AboutUs.php">About Us</a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Fork & Flame</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="Menu.php">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Booking.php">Booking</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="AboutUs.php">About us</a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
 
     <!-- Hero Section -->
