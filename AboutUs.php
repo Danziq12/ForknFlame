6eda8dd908b1b6e964881e76dc22c8c7eb06606c
Danziq12 <danish.ziqry0512@gmail.com>
2026-08-28 18:13:06 +0800

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>About Us | Fork & Flame</title>
-        <link rel="stylesheet" href="bootstrap.css">
         <link rel="stylesheet" href="about.css">
     </head>
 
