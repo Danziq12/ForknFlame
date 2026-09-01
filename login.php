942ff3da967f7b02c48cfbef3df76ed00ba7a6c5
Danziq12 <danish.ziqry0512@gmail.com>
2026-09-01 12:44:05 +0800

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
                             <input type="password" name="password" class="form-control" required>
                         </div>
 
-                        <button type="submit" class="btn btn-primary w-100">Login</button>
+                        <button type="submit" class="btn btn-primary w-100">Login & Request OTP</button>
                     </form>
 
                     <div class="text-center mt-3">
